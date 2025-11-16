099bdf77c0185417422df629efe9b5d7bd68097b
oktaviadwiputripermatasari <oktaviadwiputri506@gmail.com>
2025-11-16 21:52:45 +0700

    add: login using uname and pwd_masih error di expect

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,13 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+      launchOptions: {
+      args: ['--start-maximized'],   // buka browser dalam keadaan maximize
+      headless: false 
+      }
+    }
+      
     },
 
     // {
--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 
+/* Test case : Validation Tile*/
 test('Validation Title', async ({page}) => {
     //launch web
     await page.goto('https://opensource-demo.orangehrmlive.com/');
@@ -7,3 +8,17 @@ test('Validation Title', async ({page}) => {
     //expected result
     await expect(page).toHaveTitle('OrangeHRM');
 });
+
+/** TC-Login-001 : Login using valid username and password */
+test('Login using valid username and password', async({page})=>{
+    //launch web
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+    //get element using xpath
+    await page.locator("//input[@name='username']").fill("Admin");
+    await page.locator("//input[@name='password']").fill("admin123");
+    await page.locator("//button[@type='submit' and contains(., 'Login')]").click();
+    expect(page.locator("//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']")).toHaveClass("oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module");
+    await page.screenshot({path: "screenshot/login_sukses.png"});
+});
+
