e1fa21ef314260a77ff1ddb6b657bd096bc8717e
oktaviadpp <oktaviadwiputri506@gmail.com>
2025-11-24 09:39:28 +0700

    add: scenario login DONE

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -24,8 +24,52 @@ test('Login using valid username and password', async({page})=>{
     await username.fill("Admin");
     await password.fill("admin123");
     await btn_Login.click();
+
+    /**wait for element stable | DISCOURAGED |
     await page.waitForLoadState('networkidle');
+    */
+
+    await text_Dashboard.waitFor();
     expect(text_Dashboard).toBeVisible();
     await page.screenshot({path: "screenshot/login_sukses.png"});
 });
+
+/**TC-Login-002 : Login using invalid username and password */
+test('Login using invalid username and password', async({page}) => {
+    //launch web
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+    //variables | get element using xpath
+    const username = page.locator("//input[@name='username']");
+    const password = page.locator("//input[@name='password']");
+    const btn_Login = page.locator("//button[@type='submit']");
+    const error_msg = page.locator("//p[normalize-space()='Invalid credentials']");
+    
+    //process 
+    await username.fill("admin123");
+    await password.fill("Admin");
+    await btn_Login.click();
+
+    await error_msg.waitFor();
+    expect(error_msg).toBeVisible();
+    await page.screenshot({path: "screenshot/login_invalid username and password.png"});
+});
+
+/**TC-Login-003 : Login with empty username and password */
+test('Login with empty username and password', async({page}) => {
+    //launch web
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+    //variables | get element using xpath
+    const btn_Login = page.locator("//button[@type='submit']");
+    const error_msg_empty = page.locator("(//span[normalize-space()='Required'])[1]");
+    
+    //process 
+    await btn_Login.click();
+
+    await error_msg_empty.waitFor();
+    expect(error_msg_empty).toBeVisible();
+    await page.screenshot({path: "screenshot/login_empty input.png"});
+});
+
 
